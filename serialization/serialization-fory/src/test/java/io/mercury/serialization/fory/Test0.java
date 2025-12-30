4ebd8de50d4aa876aee662bd7315561f5aef2d51
yellow013 <wk9988@gmail.com>
2025-12-30 13:09:10 +0900

    update validator

---
+++ serialization/serialization-fory/src/test/java/io/mercury/serialization/fory/Test0.java
@@ -0,0 +1,43 @@
+package io.mercury.serialization.fory;
+
+import junit.framework.TestCase;
+import org.apache.fory.Fory;
+import org.apache.fory.config.Language;
+
+/**
+ * Unit test for simple App.
+ */
+public class Test0 extends TestCase {
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testCancelOrder() {
+        TestObject object = new TestObject();
+        object.setAccountId(1000)
+                .setOrdSysId(1000001L)
+                .setInstrumentCode("ag2412")
+                .setRemark("TEST OBJECT REMARK");
+        // Note that Fory instances should be reused between
+        // multiple serializations of different objects.
+        Fory fory = Fory.builder().withLanguage(Language.JAVA)
+                .requireClassRegistration(true)
+                .build();
+        // Registering types can reduce class name serialization overhead, but not mandatory.
+        // If class registration enabled, all custom types must be registered.
+        fory.register(TestObject.class);
+        byte[] bytes = fory.serializeJavaObject(object);
+
+        System.out.println(bytes.length);
+        System.out.println(new String(bytes));
+
+        TestObject object1 = fory.deserializeJavaObject(bytes, TestObject.class);
+
+
+        System.out.println(object);
+        System.out.println(object == object1);
+        System.out.println(object1);
+
+    }
+
+}
---
+++ serialization/serialization-json/src/main/java/io/mercury/serialization/json/TypeAdapters.java
@@ -0,0 +1,54 @@
+package io.mercury.serialization.json;
+
+public final class TypeAdapters {
+
+//    public static final JsonSerializer<Duration> DurationSerializer =
+//            (src, typeOfSrc, context) ->
+//                    (src == null) ? null
+//                            : new JsonPrimitive(src.toString());
+//
+//    public static final JsonSerializer<java.time.Instant> InstantSerializer =
+//            (src, typeOfSrc, context) ->
+//                    (src == null) ? null
+//                            : new JsonPrimitive(DateTimeFormatter.ISO_INSTANT.format(src));
+//
+//    public static final JsonSerializer<ZoneId> ZoneIdSerializer =
+//            (src, typeOfSrc, context) ->
+//                    (src == null) ? null
+//                            : new JsonPrimitive(src.getId());
+//
+//    public static final JsonSerializer<LocalDate> LocalDateSerializer =
+//            (src, typeOfSrc, context) ->
+//                    (src == null) ? null
+//                            : new JsonPrimitive(DatePattern.YY_MM_DD.getFormatter().format(src));
+//
+//    public static final JsonSerializer<LocalTime> LocalTimeSerializer =
+//            (src, typeOfSrc, context) ->
+//                    (src == null) ? null
+//                            : new JsonPrimitive(TimePattern.HH_MM_SS_SSS.getFormatter().format(src));
+//
+//    public static final JsonSerializer<LocalDateTime> LocalDateTimeSerializer =
+//            (src, typeOfSrc, context) ->
+//                    (src == null) ? null
+//                            : new JsonPrimitive(DateTimePattern.YY_MM_DD_HH_MM_SS_SSS.getFormatter().format(src));
+//
+//    public static final JsonSerializer<ZonedDateTime> ZonedDateTimeSerializer =
+//            (src, typeOfSrc, context) ->
+//                    (src == null) ? null
+//                            : new JsonPrimitive(DateTimeFormatter.ISO_DATE_TIME.format(src));
+//
+//
+//    public static final Map<Class<?>, JsonSerializer<?>> TypeAdaptorMap = new HashMap<>();
+//
+//    static {
+//        TypeAdaptorMap.put(Duration.class, DurationSerializer);
+//        TypeAdaptorMap.put(Instant.class, InstantSerializer);
+//        TypeAdaptorMap.put(ZoneId.class, ZoneIdSerializer);
+//        TypeAdaptorMap.put(LocalDate.class, LocalDateSerializer);
+//        TypeAdaptorMap.put(LocalTime.class, LocalTimeSerializer);
+//        TypeAdaptorMap.put(LocalDateTime.class, LocalDateTimeSerializer);
+//        TypeAdaptorMap.put(ZonedDateTime.class, ZonedDateTimeSerializer);
+//    }
+
+
+}
